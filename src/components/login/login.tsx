0003a8c084bb5210f192e6e981afdcea7abc6e58
Paulo H Mariano <51302275+paulodeab@users.noreply.github.com>
2024-06-25 12:19:15 -0300

    .

---
+++ src/components/login/login.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Button, Text, TextInput, View } from 'react-native';
+
+
+
+
+function Login(){
+
+
+    return (<View>
+                <View>
+                    <Text>LOGIN</Text>
+                    <TextInput />
+                    <Text>SENHA</Text>
+                    <TextInput />
+                </View>
+            </View>);
+}
+
+
+export default Login;
